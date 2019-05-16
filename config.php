bb1d43bab6cf64dd01f7a5d6030f8583c38602b1
habibkdp <habibkdp9@gmail.com>
2019-05-16 10:22:36 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "rintohsf@gmail.com";
+$password = "Kikiekzx123"";
+
+//Pillih Warna 0 untuk hijau 1 untuk kuning
+$collor = "0";
+
+// Pilih Jumlah Spin Saran 1 - 5
+$amount = "1";
+
+
+
